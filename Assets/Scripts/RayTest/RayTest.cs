9f487634c05b666a11534215745608ced35bb410
Unknown <18335929360@163.com>
2019-03-04 14:59:18 +0800

    [李瑞瑞] 3D物体拾取测试

---
+++ Assets/Scripts/RayTest/RayTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //从摄像机发射一条射线
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //撞击到哪个3D物体
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+}

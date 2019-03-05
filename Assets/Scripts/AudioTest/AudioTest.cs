89c7681905554a4bd71b743f351ec6a179236c08
Unknown <18335929360@163.com>
2019-03-05 09:58:55 +0800

    [李瑞瑞]声音组件测试

---
+++ Assets/Scripts/AudioTest/AudioTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour
+{
+    public AudioSource s;
+    // Use this for initialization
+    void Start()
+    {
+        // 声音的全局设置
+        AudioConfiguration audio_config = AudioSettings.GetConfiguration();
+        AudioSettings.Reset(audio_config);
+        // end 
+
+        // this.s.Play();
+        this.s.PlayDelayed(5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (this.s.isPlaying)
+            {
+                this.s.Pause();
+            }
+            else
+            {
+                this.s.Play();
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            this.s.Stop();
+        }
+    }
+}

be743136765c153f379d7c51d82e6cefe705a7e7
Unknown <18335929360@163.com>
2019-03-06 15:57:08 +0800

    [李瑞瑞] 水和雾测试

---
+++ Assets/Scripts/FogTest/Controler.cs
@@ -0,0 +1,40 @@
+﻿//using UnityEngine;
+//using System.Collections;
+
+//public class Controler : MonoBehaviour {
+//    public EasyJoystick MyJoystick;
+//    CharacterController controller;
+//    float RunSpeed;
+//    Vector3 MoveDrection ;
+//    public GameObject CameraA;
+//	void Start () {
+//        controller = (CharacterController)this.GetComponent("CharacterController");
+//        controller.slopeLimit = 30.0f;
+//        RunSpeed = 1000;
+//	}
+//	void Update () {
+//        MoveDrection = new Vector3(Input.GetAxis("Fire1"), 0, 0);
+//        MoveDrection = transform.TransformDirection(MoveDrection);
+//        if (MyJoystick.JoystickTouch.y == 0 && MyJoystick.JoystickTouch.x == 0) {
+//            GetComponent<Animation>().Play("Laugh");
+//        }
+//        if (MyJoystick.JoystickTouch.y > 0.5f) {
+//                GetComponent<Animation>().Play("Run");    
+//        }
+//        if (MyJoystick.JoystickTouch.x < -0.5f)
+//        {
+//            this.transform.Rotate(0, -1.0f, 0);
+//        }
+//        if (MyJoystick.JoystickTouch.x > 0.5f)
+//        {
+//            this.transform.Rotate(0, 1.0f, 0);
+//        }
+//        if (MyJoystick.JoystickTouch.y < -0.5f)
+//        {
+//            CameraA.transform.Translate(Vector3.back);
+//        }
+//        if(GetComponent<Animation>().IsPlaying("Run")){
+//            controller.SimpleMove(MoveDrection * (Time.deltaTime * RunSpeed));
+//        }
+//	}
+//}
---
+++ Assets/Scripts/FogTest/Pop.cs
@@ -0,0 +1,98 @@
+﻿    using UnityEngine;
+    using System.Collections;
+
+    public class Pop : MonoBehaviour {
+    private float Ypos1=0.0f;
+    private float Ypos2=0.0f;
+    private float Ypos3=0.0f;
+    private float Ypos4=0.0f;
+    private float Ypos5=0.0f;
+    private bool showDropdownButtons1 ;
+    private bool showDropButtonsUP1;
+    float dropSpeed   = 500.0f;
+    private string St="FogMode";    
+    void Start() {
+    }
+    void Update(){
+        if(showDropdownButtons1 == true){
+            Ypos1 += Time.deltaTime * dropSpeed;   
+            Ypos2 += Time.deltaTime * dropSpeed;   
+            Ypos3 += Time.deltaTime * dropSpeed;
+            Ypos4 += Time.deltaTime * dropSpeed;
+            if(Ypos1 >= 60){
+                Ypos1 = 60;
+            }
+            if(Ypos2 >= 120){
+                Ypos2 = 120;
+            }
+            if(Ypos3 >= 180){
+                Ypos3 = 180;
+            }
+            if(Ypos4 >= 240){
+                Ypos4 = 240;
+            }
+            if(showDropButtonsUP1 == true){
+                Ypos1 -= Time.deltaTime * dropSpeed;
+                Ypos2 -= Time.deltaTime * dropSpeed;
+                Ypos3 -= Time.deltaTime * dropSpeed;
+                Ypos4 -= Time.deltaTime * dropSpeed;
+                if(Ypos1 >= 0 || Ypos2 >= 0 || Ypos3 >= 0 || Ypos4 >= 0){
+                    Ypos1 = 0;
+                    Ypos2 = 0;
+                    Ypos3 = 0;
+                    Ypos4 = 0;
+                    showDropButtonsUP1 = false;
+                    showDropdownButtons1 = false;  
+                }
+            }
+        }
+    }
+        void OnGUI (){
+            if(showDropdownButtons1 == false){
+                if (GUI.RepeatButton (new Rect (50, 0, 200, 60), St)){
+                    showDropdownButtons1 = true;
+                }
+            }
+            if(showDropdownButtons1 == true){
+
+                if (GUI.Button(new Rect(50, 0, 200, 60), St))
+                {
+                    showDropButtonsUP1 = true;
+                    showDropdownButtons1 = false;
+                }
+
+                if (GUI.Button(new Rect(50, Ypos1, 200, 60), "Linear"))
+                {
+                    showDropButtonsUP1 = true;
+                    showDropdownButtons1 = false;
+                    RenderSettings.fogMode = FogMode.Linear;
+                    St = "Linear";
+                }
+                if (GUI.Button(new Rect(50, Ypos2, 200, 60), "Exp2"))
+                {
+
+                    showDropButtonsUP1 = true;
+                    showDropdownButtons1 = false;
+                    RenderSettings.fogMode = FogMode.ExponentialSquared;
+                    St = "Exp2";
+
+                }
+                if (GUI.Button(new Rect(50, Ypos3, 200, 60), "Exponential"))
+                {
+ 
+                    showDropButtonsUP1 = true;
+                    showDropdownButtons1 = false;
+                    RenderSettings.fogMode = FogMode.Exponential;
+                    St = "Exponential";
+                }
+                if (GUI.Button(new Rect(50, Ypos4, 200, 60), "None"))
+                {
+
+                    showDropButtonsUP1 = true;
+                    showDropdownButtons1 = false;
+                    RenderSettings.fogMode = 0;
+                    St = "None";
+                }
+            }
+        }   
+    }

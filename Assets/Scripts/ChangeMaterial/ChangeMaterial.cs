6b6b16e0e0ea08521dfaafc1f5cb6f442abceae1
Unknown <18335929360@163.com>
2019-03-07 10:30:16 +0800

    [李瑞瑞]法线贴图使用测试

---
+++ Assets/Scripts/ChangeMaterial/ChangeMaterial.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    public Material diffuseMap;
+
+    public Material normalMap;
+
+    private MeshRenderer render;
+
+    private bool normal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        normal = false;
+        render = this.GetComponent<MeshRenderer>();
+        render.materials = new Material[2] { this.diffuseMap, this.diffuseMap };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            this.normal = !normal;
+            if(normal)
+            {
+                 render.materials = new Material[2]  { this.normalMap,this.normalMap };
+            }
+            else
+            {
+                render.materials = new Material[2] { this.diffuseMap, this.diffuseMap }; 
+            }
+        }
+    }
+}
